2b55ae12f3841fbb554081d5c7221595c8c9d1eb
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-08-11 21:43:47 -0300

    Commenting and organizing the code

--- arch-linux-post-install.sh
+++ arch-linux-post-install.sh
@@ -1,291 +1,333 @@
 # !/bin/bash
 
-# clear
+# INSTALAÇÃO DO REFLECTOR
 
-# echo -e "\nInstalando e connfigurando o reflector\n"
+clear
 
-# echo -e "\nSincronizando pacotes\n"
+echo -e "\nInstalando e configurando o reflector\n"
 
-# sudo pacman -Sy
+echo -e "\nSincronizando pacotes\n"
 
-# echo -e "\nInstalando reflector\n"
+sudo pacman -Sy
 
-# sudo pacman -S reflector rsync curl --noconfirm
+echo -e "\nInstalando reflector\n"
 
-# FILE=/etc/pacman.d/mirrorlist.bak
+sudo pacman -S reflector rsync curl --noconfirm
 
-# if ! [[ -f "$FILE" ]]; then
-#     echo -e "\nCriando backup do mirrorlist para $FILE\n"
-#     sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-# fi
+FILE=/etc/pacman.d/mirrorlist.bak
 
-# sudo reflector --download-timeout 5 --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+if ! [[ -f "$FILE" ]]; then
+    echo -e "\nCriando backup do mirrorlist para $FILE\n"
+    sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+fi
 
-# clear
+sudo reflector --download-timeout 5 --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-# echo -e "\nAtualizando o sistema\n"
+# ATUALIZAÇÃO DO SISTEMA
 
-# sudo pacman -Syyu --noconfirm
+clear
 
-# clear
+echo -e "\nAtualizando o sistema\n"
 
-# echo -e "\nInstalando Git\n"
+sudo pacman -Syyu --noconfirm
 
-# sudo pacman -S git --noconfirm
+# INSTALAÇÃO DO GIT
 
-# clear
+clear
 
-# echo -e "\nPacote de ícones Papirus\n"
+echo -e "\nInstalando Git\n"
 
-# echo -e "\nClonando repositório\n"
+sudo pacman -S git --noconfirm
 
-# git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git
+# INSTALAÇÃO DO PACOTE DE ÍCONES PAPIRUS
 
-# cd papirus-icon-theme
+clear
 
-# echo -e "\nSalvando arquivos no diretório /icons/\n"
+echo -e "\nPacote de ícones Papirus\n"
 
-# FILE=$HOME/.local/share/icons/
+echo -e "\nClonando repositório\n"
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git
 
-# mkdir $HOME/.local/share/icons/
+cd papirus-icon-theme
 
-# mv *Papirus* $HOME/.local/share/icons/
+echo -e "\nSalvando arquivos no diretório /icons/\n"
 
-# ls $HOME/.local/share/icons/
+FILE=$HOME/.local/share/icons/
 
-# cd ../
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# FILE=papirus-icon-theme
+mkdir $HOME/.local/share/icons/
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+mv *Papirus* $HOME/.local/share/icons/
 
-# echo -e "\nAlterando ícones do sistema\n"
+ls $HOME/.local/share/icons/
 
-# xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus"
+cd ../
 
-# clear
+FILE=papirus-icon-theme
 
-# echo -e "\nTema Orchis\n"
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# FILE=Orchis-theme
+echo -e "\nAlterando ícones do sistema\n"
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus"
 
-# git clone https://github.com/vinceliuice/Orchis-theme.git
+# INSTALAÇÃO DO TEMA ORCHIS
 
-# clear
+clear
 
-# echo -e "\nInstalando Git\n"
+echo -e "\nTema Orchis\n"
 
-# sudo pacman -S tar --noconfirm
+FILE=Orchis-theme
 
-# clear
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# echo -e "\nSalvando arquivos no diretório /themes/\n"
+git clone https://github.com/vinceliuice/Orchis-theme.git
 
-# ls
+# INSTALAÇÃO DO TAR
 
-# cd Orchis-theme/release/
+clear
 
-# FILE=$HOME/.local/share/themes/
+echo -e "\nInstalando Tar\n"
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+sudo pacman -S tar --noconfirm
 
-# mkdir $HOME/.local/share/themes/
+echo -e "\nSalvando arquivos no diretório /themes/\n"
 
-# tar -xf Orchis.tar.xz -C $HOME/.local/share/themes/
+ls
 
-# ls $HOME/.local/share/themes/
+cd Orchis-theme/release/
 
-# cd ../../
+FILE=$HOME/.local/share/themes/
 
-# FILE=Orchis-theme
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+mkdir $HOME/.local/share/themes/
 
-# echo -e "\nAlterando tema do sistema\n"
+tar -xf Orchis.tar.xz -C $HOME/.local/share/themes/
 
-# xfconf-query -c xsettings -p /Net/ThemeName -s "Orchis-Dark"
+ls $HOME/.local/share/themes/
 
-# clear
+cd ../../
 
-# echo -e "\nInstalação do yay\n"
+FILE=Orchis-theme
 
-# echo -e "\nInstalando Go\n"
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# sudo pacman -S go base-devel --noconfirm
+echo -e "\nAlterando tema do sistema\n"
 
-# FILE=yay
+xfconf-query -c xsettings -p /Net/ThemeName -s "Orchis-Dark"
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+# INSTALAÇÃO DO YAY
 
-# echo -e "\nClonando repositório\n"
+clear
 
-# git clone https://aur.archlinux.org/yay.git
+echo -e "\nInstalação do yay\n"
 
-# cd yay
+echo -e "\nInstalando Go\n"
 
-# echo -e "\nInstalando yay\n"
+sudo pacman -S go base-devel --noconfirm
 
-# makepkg -si --noconfirm
+FILE=yay
 
-# cd ../
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# FILE=yay
+echo -e "\nClonando repositório\n"
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+git clone https://aur.archlinux.org/yay.git
 
-# echo -e "\nRemovendo Go\n"
+cd yay
 
-# sudo pacman -Rns go --noconfirm
+echo -e "\nInstalando yay\n"
 
-# clear
+makepkg -si --noconfirm
 
-# echo -e "\nInstalando pacotes utilizados\n"
+cd ../
 
-# sudo pacman -S blender discord docker docker-compose firefox gamemode gcc gcc-fortran gimp git gnu-free-fonts gpick gsfonts gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gzip inkscape jq jre-openjdk libreoffice-still make nano neofetch noto-fonts npm obs-studio opendesktop-fonts pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer python-pip spotify-launcher telegram-desktop ttf-anonymous-pro ttf-arphic-ukai ttf-arphic-uming ttf-baekmuk ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-dejavu ttf-droid ttf-eurof ttf-fantasque-sans-mono ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-hack ttf-hanazono ttf-hannom ttf-ibm-plex ttf-inconsolata ttf-indic-otf ttf-input ttf-ionicons ttf-iosevka-nerd ttf-jetbrains-mono ttf-joypixels ttf-junicode ttf-khmer ttf-lato ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-monoid ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean ttf-roboto ttf-roboto-mono ttf-sarasa-gothic ttf-sazanami ttf-tibetan-machine ttf-ubuntu-font-family inter-font unrar unzip virt-manager vi vim virtualbox vlc wget xfce4-screenshooter xfce4-screensaver xfce4-taskmanager yarn zip zsh --noconfirm
+FILE=yay
 
-# clear
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# echo -e "\nInstalando pacotes utilizados com o yay\n"
+echo -e "\nRemovendo Go\n"
 
-# echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" $PKGNAME
+sudo pacman -Rns go --noconfirm
 
-# yay -S google-chrome insomnia-bin beekeeper-studio-bin masterpdfeditor-free notion-app onlyoffice-bin ttf-ms-fonts visual-studio-code-bin microsoft-edge
+# INSTALAÇÃO DE PACOTES UTILIZADOS VIA PACMAN
 
-# clear
+clear
 
-# echo -e "\nInstalando Steam\n"
+echo -e "\nInstalando pacotes utilizados\n"
 
-# echo -e "\nEditando /etc/pacman.conf\n"
+sudo pacman -S blender discord docker docker-compose firefox gamemode gcc gcc-fortran gimp git gnu-free-fonts gpick gsfonts gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gzip inkscape jq jre-openjdk libreoffice-still make nano neofetch noto-fonts npm obs-studio opendesktop-fonts pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer python-pip spotify-launcher telegram-desktop ttf-anonymous-pro ttf-arphic-ukai ttf-arphic-uming ttf-baekmuk ttf-bitstream-vera ttf-caladea ttf-carlito ttf-cascadia-code ttf-cormorant ttf-croscore ttf-dejavu ttf-droid ttf-eurof ttf-fantasque-sans-mono ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-hack ttf-hanazono ttf-hannom ttf-ibm-plex ttf-inconsolata ttf-indic-otf ttf-input ttf-ionicons ttf-iosevka-nerd ttf-jetbrains-mono ttf-joypixels ttf-junicode ttf-khmer ttf-lato ttf-liberation ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-monoid ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean ttf-roboto ttf-roboto-mono ttf-sarasa-gothic ttf-sazanami ttf-tibetan-machine ttf-ubuntu-font-family inter-font unrar unzip virt-manager vi vim virtualbox vlc wget xfce4-screenshooter xfce4-screensaver xfce4-taskmanager yarn zip zsh --noconfirm
 
-# sed -z 's/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/' /etc/pacman.conf >tmp.$$.txt && sudo mv tmp.$$.txt /etc/pacman.conf
+# INSTALAÇÃO DE PACOTES UTILIZADOS DO AUR
 
-# echo -e "\nAtualizando lista de pacotes\n"
+clear
 
-# sudo pacman -Sy
+echo -e "\nInstalando pacotes utilizados com o yay\n"
 
-# echo -e "\nInstalando Steam\n"
+echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" $PKGNAME
 
-# sudo pacman -S steam --noconfirm
+yay -S google-chrome insomnia-bin beekeeper-studio-bin masterpdfeditor-free notion-app onlyoffice-bin ttf-ms-fonts visual-studio-code-bin microsoft-edge
 
-# echo -e "\nInstalando Zsh\n"
+# INSTALAÇÃO DA STEAM
 
-# sudo pacman -S zsh zsh-completions --noconfirm
+clear
 
-# echo -e "\nInstalando Oh My Zsh\n"
+echo -e "\nInstalando Steam\n"
 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e "\nEditando /etc/pacman.conf\n"
 
-# echo -e "\nInstalando tema Spaceship\n"
+sed -z 's/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/' /etc/pacman.conf >tmp.$$.txt && sudo mv tmp.$$.txt /etc/pacman.conf
 
-# FILE=$ZSH_CUSTOM/themes/spaceship-prompt
+echo -e "\nAtualizando lista de pacotes\n"
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+sudo pacman -Sy
 
-# sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+echo -e "\nInstalando Steam\n"
 
-# echo -e "\nCriando link simbólico para pasta do Spaceship\n"
+sudo pacman -S steam --noconfirm
 
-# FILE=$ZSH_CUSTOM/themes/spaceship.zsh-theme
+# INSTALAÇÃO DO ZSH
 
-# if [[ -d "$FILE" ]]; then
-#     echo -e "\nRemovendo $FILE existente\n"
-#     sudo rm -r $FILE
-# fi
+clear
 
-# sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+echo -e "\nInstalando Zsh\n"
 
-# echo -e "\nAplicando tema Spaceship\n"
+sudo pacman -S zsh zsh-completions --noconfirm
 
-# sudo sed -i 's/ZSH_THEME=.*/ZSH_THEME="spaceship"/g' ~/.zshrc
+# INSTALAÇÃO DO OH MY ZSH
 
-# echo -e "\nConfigurando o Spaceship\n"
+echo -e "\nInstalando Oh My Zsh\n"
 
-# echo 'SPACESHIP_PROMPT_ORDER=(
-#   user          # Username section
-#   dir           # Current directory section
-#   host          # Hostname section
-#   git           # Git section (git_branch + git_status)
-#   hg            # Mercurial section (hg_branch  + hg_status)
-#   exec_time     # Execution time
-#   line_sep      # Line break
-#   vi_mode       # Vi-mode indicator
-#   jobs          # Background jobs indicator
-#   exit_code     # Exit code section
-#   char          # Prompt character
-# )
-# SPACESHIP_USER_SHOW=always
-# SPACESHIP_PROMPT_ADD_NEWLINE=false
-# SPACESHIP_CHAR_SYMBOL="❯"
-# SPACESHIP_CHAR_SUFFIX=" "' >> ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# echo -e "\nPlugins do Zsh\n"
+# INSTALAÇÃO DO TEMA SPACESHIP
 
-# echo -e "\nInstalando ZInit\n"
+echo -e "\nInstalando tema Spaceship\n"
 
-# bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+FILE=$ZSH_CUSTOM/themes/spaceship-prompt
 
-# echo -e "\nConfigurando ZInit\n"
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# echo 'zinit light zdharma/fast-syntax-highlighting
-# zinit light zsh-users/zsh-autosuggestions
-# zinit light zsh-users/zsh-completions
-# ' >> ~/.zshrc
+sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
-# FILE=$HOME/.config/Code/User/settings.json
+echo -e "\nCriando link simbólico para pasta do Spaceship\n"
 
-# if ! [[ -f "$FILE" ]]; then
-#     echo -e "\nCriando settings.json em $FILE\n"
-#     echo '{
-#   "git.enableSmartCommit": true,
-#   "git.confirmSync": false
-# }' >>$FILE
-# fi
+FILE=$ZSH_CUSTOM/themes/spaceship.zsh-theme
 
-# echo -e "\nDefinindo a fonte do terminal integrado do VS Code\n"
+if [[ -d "$FILE" ]]; then
+    echo -e "\nRemovendo $FILE existente\n"
+    sudo rm -r $FILE
+fi
 
-# jq '. + { "terminal.integrated.fontFamily": "Fira Code Retina" }' $HOME/.config/Code/User/settings.json >tmp.$$.json && mv tmp.$$.json $HOME/.config/Code/User/settings.json
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-# echo -e "\nDefinindo o zsh como shell padrão do terminal integrado do VS Code\n"
+# CONFIGURAÇÃO DO SPACESHIP
 
-# jq '. + { "#terminal.integrated.shell.linux": "/bin/zsh" }' $HOME/.config/Code/User/settings.json >tmp.$$.json && mv tmp.$$.json $HOME/.config/Code/User/settings.json
+echo -e "\nAplicando tema Spaceship\n"
 
-# clear
+sudo sed -i 's/ZSH_THEME=.*/ZSH_THEME="spaceship"/g' ~/.zshrc
 
-# echo -e "\nHabilitando tela de bloqueio\n"
+echo -e "\nConfigurando o Spaceship\n"
 
-# xfconf-query -c xfce4-session -p /general/LockCommand -s "xlock" --create -t string
+echo 'SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  line_sep      # Line break
+  vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "' >>~/.zshrc
 
-# xfconf-query -c xfce4-session -p /general/LockCommand -s "xlock"
+echo -e "\nPlugins do Zsh\n"
 
-# xfce4-screensaver &
+# INSTALAÇÃO DO ZINIT
+
+echo -e "\nInstalando ZInit\n"
+
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+# CONDIGURAÇÃO DO ZINIT
+
+echo -e "\nConfigurando ZInit\n"
+
+echo 'zinit light zdharma/fast-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
+' >>~/.zshrc
+
+clear
+
+# DEFINIÇÃO DA FONTE DO TERMINAL INTEGRADO DO VSCODE
+
+FILE=$HOME/.config/Code/User/settings.json
+
+echo -e "\nDefinindo a fonte do terminal integrado do VS Code\n"
+
+if ! [[ -f "$FILE" ]]; then
+    echo -e "\nCriando settings.json em $FILE\n"
+    echo '{
+  "git.enableSmartCommit": true,
+  "git.confirmSync": false
+}' >>$FILE
+fi
+
+jq '. + { "terminal.integrated.fontFamily": "Fira Code Retina" }' $HOME/.config/Code/User/settings.json >tmp.$$.json && mv tmp.$$.json $HOME/.config/Code/User/settings.json
+
+# DEFININDO ZSH COMO SHELL DO VSCODE
+
+echo -e "\nDefinindo o zsh como shell padrão do terminal integrado do VS Code\n"
+
+jq '. + { "#terminal.integrated.shell.linux": "/bin/zsh" }' $HOME/.config/Code/User/settings.json >tmp.$$.json && mv tmp.$$.json $HOME/.config/Code/User/settings.json
+
+# HABILITAÇÃO DA TELA DE BLOQUEIO
+
+clear
+
+echo -e "\nHabilitando tela de bloqueio\n"
+
+xfconf-query -c xfce4-session -p /general/LockCommand -s "xlock" --create -t string
+
+xfconf-query -c xfce4-session -p /general/LockCommand -s "xlock"
+
+xfce4-screensaver &
+
+# INSTALAÇÃO DAS EXTENSÕES DO VSCODE
 
 clear
 
